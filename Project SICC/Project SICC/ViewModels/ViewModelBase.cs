5544217ee4217b7e10a1e20af7a38de8b8137057
EvixWasTaken <piotr5wwwwwz@gmail.com>
2025-06-07 20:29:25 +0200

    Push

---
+++ Project SICC/Project SICC/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Project_SICC.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ Project SICC/Project SICC/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Project_SICC.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
